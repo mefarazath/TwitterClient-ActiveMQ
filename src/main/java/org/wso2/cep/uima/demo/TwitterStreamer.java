dcb9901243c75c011fc63616c609dd4b1de51b5a
Farasath Ahamed <mefarazath@gmail.com>
2015-01-27 11:07:55 +0530

    adding dual mode twitter client

---
+++ src/main/java/org/wso2/cep/uima/demo/TwitterStreamer.java
@@ -0,0 +1,142 @@
+package org.wso2.cep.uima.demo;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.wso2.cep.uima.demo.Util.TwitterConfiguration;
+import org.wso2.cep.uima.demo.Util.TwitterConfigurationBuiler;
+import org.xml.sax.SAXException;
+import twitter4j.FilterQuery;
+import twitter4j.Logger;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import javax.jms.JMSException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class TwitterStreamer {
+	
+    // API keys for the TwitterStreaming API
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessToken;
+    private String accessTokenSecret;
+
+    // Configuration parameters of the ActiveMQ Message Broker
+    private String JMSUrl;
+
+    private ConfigurationBuilder cb;
+    private TwitterStream twitterStream;
+    private FilterQuery filter;
+
+    private final long[] usersToFilter; // userIDs of the users to filter when the streaming tweets
+
+    private static Logger logger = Logger.getLogger(TwitterStreamer.class);
+
+    public static void main(String[] args) throws JMSException, IOException,
+                                            SAXException, ParserConfigurationException {
+
+        String JMSUrl = args[0];
+        String queueName = args[1];
+
+	if(JMSUrl.equals("") || queueName.equals("")){
+		System.out.println("Usage ant -DjmsUrl=<JMS_URL> -DqueueName=<QUEUE_NAME>");
+		System.exit(0);
+	}
+
+        TwitterConfiguration config = TwitterConfigurationBuiler.getTwitterConfiguration();
+
+        // create a streamer object
+        TwitterStreamer streamer = new TwitterStreamer(config.getFollowers(), JMSUrl);
+        streamer.setConsumerSecret(config.getConsumerSecret());
+        streamer.setConsumerKey(config.getConsumerKey());
+        streamer.setAccessToken(config.getAccessToken());
+        streamer.setAccessTokenSecret(config.getAccessTokenSecret());
+
+        // create the status handler of the stream
+        StatusHandler statusHandler = new StatusHandler(JMSUrl, queueName);
+
+        // start streaming for tweets
+        streamer.startStream(statusHandler);
+    }
+
+    /***
+     *
+     * @param users long[] userIDs of the users to filer the stream for
+     * @param JMSUrl String url of the ActiveMQ Server
+     */
+    public TwitterStreamer(long[] users, String JMSUrl){
+        this.JMSUrl = JMSUrl;
+        this.usersToFilter = users.clone();
+        PropertyConfigurator.configure("src/main/resources/log4j.properties");
+    }
+
+
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setAccessTokenSecret(String accessTokenSecret) {
+        this.accessTokenSecret = accessTokenSecret;
+    }
+
+    /***
+     *  Method to set up the API keys for the configuration builder
+     */
+    private void buildConfiguration(){
+        cb = new ConfigurationBuilder();
+        logger.debug("Building Configuration");
+
+        if(consumerKey == null || consumerSecret == null || accessToken == null || accessTokenSecret == null){
+            logger.error("Twitter API keys not set properly in twitterConfig.xml");
+            throw new NullPointerException("TWitter API Keys not set");
+        }
+
+        cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey(consumerKey)
+    	  .setOAuthConsumerSecret(consumerSecret)
+    	  .setOAuthAccessToken(accessToken)
+    	  .setOAuthAccessTokenSecret(accessTokenSecret);	
+    }
+
+
+    /***
+     *  Method to Set up the filter for the Streaming API
+     */
+    private void createFilter(){
+    	if(usersToFilter == null)
+    		throw new NullPointerException("User list to follow not set");
+    	
+    	filter = new FilterQuery();
+    	filter.follow(usersToFilter);
+
+    }
+
+    /***
+     * Method to start the streaming of Tweets
+     * @param handler
+     */
+    public void startStream(StatusHandler handler){
+
+        buildConfiguration();
+        TwitterStreamFactory tf = new TwitterStreamFactory(cb.build());
+        twitterStream = tf.getInstance();
+        twitterStream.addListener(handler);
+        createFilter();
+
+    	if(filter == null){
+    		throw new NullPointerException("Filter Not Set");
+    	}
+    	twitterStream.filter(filter);
+    }
+    
+
+}
---
+++ src/main/java/org/wso2/cep/uima/demo/Util/TwitterConfigurationBuiler.java
@@ -0,0 +1,108 @@
+package org.wso2.cep.uima.demo.Util;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.System.exit;
+
+/**
+ * Created by Vidura on 1/23/15.
+ */
+
+public class TwitterConfigurationBuiler {
+    private static String CONFIGURATION_FILE = "twitterConfig.xml";
+    private static Logger logger = Logger.getLogger(TwitterConfigurationBuiler.class);
+
+
+    private TwitterConfigurationBuiler(){
+
+    }
+
+    /***
+     * Method to parse and return the twitter keys configuration file
+     * @return TwitterConfiguration twitter configuration object with relevant parameters set
+     */
+    public static TwitterConfiguration getTwitterConfiguration(){
+
+        TwitterConfiguration twitterConfiguration = new TwitterConfiguration();
+        File xmlFile = new File(CONFIGURATION_FILE);
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            logger.error("Error When Configuring the parser for "+TwitterConfigurationBuiler.class.getName(),e);
+            exit(1);
+        }
+        Document doc = null;
+
+        try {
+            doc = dBuilder.parse(xmlFile);
+        } catch (IOException e) {
+            logger.error("Error Reading the Twitter Configuration File : twitterConfig.xml ",e);
+            exit(1);
+        } catch (SAXException e) {
+            logger.error("Error Parsing Twitter Configuration File : twitterConfig.xml ",e);
+            exit(1);
+        }
+
+        doc.getDocumentElement().normalize();
+
+        //get API keys
+        NodeList nodeList = doc.getElementsByTagName("TwitterAPIKeys");
+        Node nNode = nodeList.item(0);
+
+        Element eElement = (Element) nNode;
+        twitterConfiguration.setConsumerKey(eElement.getElementsByTagName("consumerKey").item(0).getTextContent());
+        twitterConfiguration.setConsumerSecret(eElement.getElementsByTagName("consumerSecret").item(0).getTextContent());
+        twitterConfiguration.setAccessToken(eElement.getElementsByTagName("accessToken").item(0).getTextContent());
+        twitterConfiguration.setAccessTokenSecret(eElement.getElementsByTagName("accessTokenSecret").item(0).getTextContent());
+
+        //get user to search for
+        NodeList nodeList2 = doc.getElementsByTagName("TwitterConfiguration");
+        Node userToSearchNode = nodeList2.item(0);
+        Element userToSearchElement = (Element) userToSearchNode;
+        twitterConfiguration.setUserToSearch(userToSearchElement.getElementsByTagName("userToSearch").item(0).getTextContent());
+
+        NodeList nodeList3 = doc.getElementsByTagName("StreamingConfiguration");
+        Node followersNode = nodeList3.item(0);
+        Element followersElement = (Element) followersNode;
+        String strFollowers = followersElement.getElementsByTagName("followers").item(0).getTextContent();
+        twitterConfiguration.setFollowers(getFollowers(strFollowers));
+
+        return twitterConfiguration;
+    }
+
+
+    /***
+     * Utility Method to get followers IDs as an Array from a comma separated String
+     * @param followers String - comma separated String of IDs to follow in the stream
+     * @return long[] Long array of IDs of users to follow
+     */
+    private static long[] getFollowers(String followers){
+
+        String[] split = followers.split(",");
+        long[] followerId = new long[split.length];
+
+        for (int i = 0; i < split.length; i++){
+            followerId[i] = Long.parseLong(split[i].trim());
+        }
+        return followerId;
+    }
+    
+
+}
+
+
